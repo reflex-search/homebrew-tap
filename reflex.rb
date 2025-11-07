cc066cf0ec40e84753ba16cda75114554455b9b8
therecluse26 <therecluse26@users.noreply.github.com>
2025-11-07 21:55:41 +0000

    reflex 0.4.2

--- reflex.rb
+++ reflex.rb
@@ -1,15 +1,15 @@
 class Reflex < Formula
   desc "Local-first, structure-aware code search engine for AI agents"
   homepage "https://github.com/reflex-search/reflex"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT OR Apache-2.0"
 
   if Hardware::CPU.arm?
     url "https://github.com/reflex-search/reflex/releases/download/v0.4.1/reflex-search-aarch64-apple-darwin.tar.xz"
     sha256 "30285dbbd6db8d73fd238582bbf18f4cf99e3576463e6085e05a86b36c6f6329"
   elsif Hardware::CPU.intel?
-    url "https://github.com/reflex-search/reflex/releases/download/v0.4.1/reflex-search-x86_64-apple-darwin.tar.xz"
-    sha256 "3c27e0c1e43c3312a910f2634b41c84909f96f637357a96280aa11c0c80a29cc"
+    url "https://github.com/reflex-search/reflex/releases/download/v0.4.2/reflex-search-x86_64-apple-darwin.tar.xz"
+    sha256 "88a600bde03afeb10ce169108a070ea520f40e0a41fdbb85119bf4b7a907a106"
   end
 
   def install
